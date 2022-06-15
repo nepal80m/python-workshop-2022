bdba425d013407c69630a3dfa2935d9c130c3289
Asim Nepal <gnepal80m@gmail.com>
2022-06-15 23:50:20 +0545

    Day 2 codes for 1st year updated

---
+++ DAY 2/1st Year Class/datatypes_and_loop.py
@@ -0,0 +1,81 @@
+# # datatypes ------
+
+# # # strings
+# # name = "ram"
+
+# # print(name)
+
+# # # list
+
+# list = ["ram ", "hari", "sita"]
+# print(list[0])
+
+# # # dictionary
+# dic = {"one": "ram", "two": "hari", 3: "sita"}
+# print(dic['one'])
+
+# # # question on dictionary
+# # #make a dictionary on your id card
+# myidcard = {
+#     "name": 'ram ',
+#     "roll_no": "LEC-075-BCT-021",
+#     ...
+# }
+
+
+# LOOPS
+# 1) if else
+#   -if else condition
+n = 4
+# if n > 3:
+#     print("greater than 3")
+
+
+#   -nested if else condition
+# if n % 2 == 0:
+#     if n > 3:
+#         print("even and greater than 3")
+#     else:
+#         print("even and less than 3")
+
+#   -else if condition
+# i = 5
+# if i > 0:
+#     print("postive")
+# elif i == 0:
+#     print("zero")
+# else:
+#     print("negative")
+
+# question 2
+# enter marks from user as an input and find he/she is pass or fail
+# pass marks equal or greater than 40
+
+# hint : dont forget type casting on input
+
+
+# 2) while loop
+
+n = "string"
+i = 5
+
+while i > 0:
+    print(n)
+    --i
+
+# question 3
+#  find sum of even no from 1 to 100
+# variabe sum =0 i =100
+# hint i%2 == 0  even no.
+    # sum = sum + i
+
+# i = i + 1
+# ++i
+# i += 1
+
+# Assignment!!!!
+# enter a year determine if it is leap year or not
+# hint: should be divisible by 4 and should not be divisible by 100
+# or be divisible by 400
+
+# 3) for loop
