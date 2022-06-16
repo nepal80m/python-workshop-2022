0860880ccddef86c8783a95f26fb24d24cc3fe44
Ram Sthapit <ramsthapit50@gmail.com>
2022-06-16 21:02:53 +0545

    day3 code for 1st year

---
+++ DAY 3/1st Year Class/string.py
@@ -0,0 +1,53 @@
+# Slicing And dicing with strings manipulation
+
+# a = "rAm sThapIt"
+
+# # print(a[0])
+# # print(a[-1])
+# # print(a[10])
+# # print(a[0:5])# here 0 is starting position and 5 is end position 
+# # it prints up to 4rth positon if 5 is the end position
+# # print(a[:5])
+# # print(a[:7:2])
+# # print(a[::2]) # here it takes the default zero value 
+# # print(a[::-1]) # reverse string
+
+# # a = "rAm sThapIt"
+
+# # print(len(a))
+
+# # string manipulation
+# txt='we arE LeArnIng PyTHon'
+
+# # print(txt.upper())
+# # print(txt.lower())
+# # print(txt.swapcase())
+# # print(txt.capitalize())
+# # print(txt.title())
+# # print(txt.split())
+# # print(txt.split("e"))
+
+# txt = '   we arE LeArnIng PyTHon   '
+
+# print(txt.replace(" ","@"))
+# print(txt)
+# print(txt.strip())
+# print(txt.rstrip())
+# print(txt.lstrip())
+
+# txt = ' we arE LeArnIng PyTHon '
+
+# print(txt.find("arE"))
+
+# string concatination
+a="mohammed "
+b="Taukir"
+c=a+b
+print(c)
+print(a+"sheikh")
+
+# Day 3 Assignment 
+# Q1: Using For loop add 2 matrix of any order
+# Q2: input="this is my third day of python learning"
+#     and the output should be 
+#     output = "This-is-my-third-day-of-python-learning"
